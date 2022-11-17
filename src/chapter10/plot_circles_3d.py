e813429e69a08b46a59bee51aac8fe993c428197
Simone Gramsch <simone.gramsch@itwm.fraunhofer.de>
2022-11-17 08:40:40 +0100

    wip chapter03, sec03

---
+++ src/chapter10/plot_circles_3d.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import make_circles
+
+X,y = make_circles(100, random_state=0, factor=0.3, noise=0.1)
+
+import numpy as np
+
+X1 = X[:,0]
+X2 = X[:,1]
+
+X3 = np.sqrt( X1**2 + X2**2 )
+
+import plotly.express as px
+
+fig = px.scatter_3d(x=X1, y=X2, z=X3, color=y)
+fig.write_html("circles_3d.html")
